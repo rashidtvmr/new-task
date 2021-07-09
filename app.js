da7991dbbf32a78efe79503d9931918471ee5a57
rashidtvmr <rashidtvmr@gmail.com>
2021-07-09 07:47:50 +0530

    env added

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const URI =
-  "mongodb+srv://rashidtvmr:Mass94877348@mycluster-ztbvh.mongodb.net/newtask";
-const localURI = "mongodb://localhost:27017/newtask";
+require('dotenv').config()
+const URI =process.env.MONGODB_ATLAS_URI
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const basicRoute = require("./Route/basic");
 const userRoute = require("./Route/user");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
